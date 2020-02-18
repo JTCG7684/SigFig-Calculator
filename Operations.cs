b27b8d2af2dd4eec0187accae7ffd271540b137d
JTCG7684 <59671433+JTCG7684@users.noreply.github.com>
2020-02-18 15:22:16 -0500

    First set of changes

---
+++ Operations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace operations 
+{
+    class Operations 
+    {
+        public void Addition() {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nFirst, is your first number a decimal or a whole number? Just type 'whole' for a whole number.");
+            Thread.Sleep(2000);
+            string numType = Console.ReadLine();
+            if (numType == "decimal" || numType == "DECIMAL" || numType == "Decimal") {
+                Console.WriteLine("\nWhat is your first decimal number?");
+                try
+                {
+                    
+                }
+                catch (Exception)
+                {
+
+                    Addition();
+                }
+            }
+            
+            Console.WriteLine("\nThis is just to test a line of code. It outputted {0}");
+        }
+
+        public void Subtraction() {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nYour answer is...Nothing yet! This is just a test message.");
+        }
+
+        public void Multiplication() 
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nYour answer is...Nothing yet! This is just a test message.");
+        }
+
+        public void Division() {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\nYour answer is...Nothing yet! This is just a test message.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using operations;
+using System.Threading;
 
 namespace SigFig_Calculator
 {
@@ -10,6 +12,59 @@ namespace SigFig_Calculator
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nWelcome User! You are now using the Significant Figures Calculator!");
+            Thread.Sleep(3000);
+            Console.WriteLine("\nThis calculator will add, subtract, multiply, or divide, while keeping into consideration the amount of significant figures neccessary!");
+            Thread.Sleep(3000);
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\nWhat operation would you like to do? You can do the type the 4 operations I mentioned, and any upper, lower, or cap variation (IE add, ADD, Add)");
+                string decision = Console.ReadLine();
+                if (decision == "Add" || decision == "add" || decision == "ADD")
+                {
+                    Operations a = new Operations();
+                    a.Addition();
+                    Thread.Sleep(3000);
+                    continue;
+                }
+                else if (decision == "Subtract" || decision == "subtract" || decision == "SUBTRACT")
+                {
+                    Operations s = new Operations();
+                    s.Subtraction();
+                    Thread.Sleep(3000);
+                    continue;
+                }
+                else if (decision == "Divide" || decision == "divide" || decision == "DIVIDE")
+                {
+                    Operations d = new Operations();
+                    d.Division();
+                    Thread.Sleep(3000);
+                    continue;
+                }
+                else if (decision == "Multiply" || decision == "multiply" || decision == "MULTIPLY")
+                {
+                    Operations m = new Operations();
+                    m.Multiplication();
+                    Thread.Sleep(3000);
+                    continue;
+                }
+                else if (decision == "quit" || decision == "Quit" || decision == "QUIT")
+                {
+                    Console.WriteLine("\nHave a great day!");
+                    Console.WriteLine("Press any button to quit...");
+                    Console.ReadLine();
+                    break;
+                }
+                else {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("\nI'm sorry, but that is not a valid choice...");
+                    Thread.Sleep(3000);
+                    continue;
+                }
+            }
+
         }
     }
 }
